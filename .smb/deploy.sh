37d9109aac67fc07820eeb6ae33cf01b5ac3f059
Seto Elkahfi <seto.elkahfi@nentgroup.com>
2025-04-18 11:47:47 +0200

    add config files

---
+++ .smb/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "------------------------  STARTING DEPLOYMENT  ------------------------"
+
+# Check if the first argument is a directory
+if [ ! -d "$1" ]
+then
+    echo "😩 $1 is not a directory."
+    echo "🛑 Deployment failed."
+    exit 1
+fi
+
+# Change to working directory
+cd $1
+
+source ~/.profile
+nvm use
+pm2 restart 0 --update-env > /dev/null 2>&1 &
+
+echo "------------------------  DEPLOYED  ------------------------"
